cfcd992ca069ffed9cffcf6bc09f0e63d2b59551
Robert Chuchro <rchuchro@groupon.com>
2012-06-13 14:36:42 -0500

    user model with password and show user page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Robert Chuchro"
+  user.email                 "Robert.Chuchro@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
