5ab9a827b2ef3651515975592e6aac932bc19a34
Robert Chuchro <rchuchro@groupon.com>
2012-06-14 00:26:02 -0500

    completed sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
 
+  get "sessions/new"
+
 	resources :users
+	resources :sessions, :only => [:new, :create, :destroy]
 
   get "pages/home"
 
@@ -9,6 +12,8 @@ SampleApp::Application.routes.draw do
   get "pages/about"
 
 	match '/signup',  :to => 'users#new'
+	match '/signin',  :to => 'sessions#new'
+	match '/signout', :to => 'sessions#destroy'
 
 	match '/contact', :to => 'pages#contact'
 	match '/about',   :to => 'pages#about'
